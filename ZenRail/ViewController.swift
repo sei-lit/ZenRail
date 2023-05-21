4b753a40a0429756185d8b33a644c34a781a4cee
SeiOmori <sei.omori.22@gmail.com>
2023-05-21 16:23:36 +0900

    Initial Commit

---
+++ ZenRail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZenRail
+//
+//  Created by 大森青 on 2023/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
